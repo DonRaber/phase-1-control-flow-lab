6bf47dce119e403b19e2623c9823ff5bad8de1f9
Donovan Raber <donovan.raber@outlook.com>
2023-08-21 08:27:34 -0700

    Control Flow

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result
+  if (distance <= 400) {
+    result = "This one is on me!";
+  } else if (distance <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (distance <= 2500) {
+    result = "I will gladly take your thirty bucks."
+  } else {
+    result = "No can do."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let destination = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return destination
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+    break;
+  }
+  return response
 }
